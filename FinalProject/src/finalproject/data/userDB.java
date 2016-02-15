28309458e79abb0bae4bd54bfda8dcc436259296
Brett Holman <brettholman91@gmail.com>
2016-02-14 21:50:12 -0800

    Added more definition to the userDB and the User class. The user class now matches the db table.

--- FinalProject/src/finalproject/data/userDB.java
+++ FinalProject/src/finalproject/data/userDB.java
@@ -7,14 +7,197 @@ import javax.naming.*;
 import java.util.*;
 
 public class userDB {
-	private static String dbURL = "jdbc:mysql://localhost:3306/CSS490";
+	private static String dbURL = "jdbc:mysql://localhost:5500/CSS490";
 	private static String dbUser = "css490";
 	private static String dbPass = "css490pass";
+	private static Calendar cal = Calendar.getInstance();
 	
-	public static User getUser(String name)
+	public static User getUser(String uName)
 	{
-		User u = new User();
-		u.setName("Anonymous");
-		return u;
+		User user = new User();
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		if(uName == null || uName.length() == 0)
+			return null;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbUser, dbPass);
+			
+			String query = "select * from users where userName = '?'";
+			stmt = conn.prepareStatement(query);
+			stmt.setString(1, uName);
+			
+			rs = stmt.executeQuery();
+			
+			if(rs == null) {
+				return null;
+			}
+				
+			user.setfName(rs.getNString("fName"));
+			user.setlName(rs.getNString("lName"));
+			user.setEmail(rs.getString("email"));
+			user.setIsAdmin(rs.getBoolean("isAdmin"));
+			user.setUserName(rs.getNString("userName"));
+			user.setLastLogin(rs.getDate("lastLogin"));
+			user.setAccountCreated(rs.getDate("accountCreated"));
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			user = null;
+		}
+		finally {
+			closeAll(stmt, conn, rs);
+		}
+		return user;
+	}
+	
+	public static boolean insertUser(User user)
+	{
+		boolean result = false;
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		
+		if(user == null)
+			return result;
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbUser, dbPass);
+			
+			String query = "insert into users(userName, fName, lName, email, pass, lastLogin, accountCreated)" +
+											"values('?', '?', '?', '?', '?', '?', '?')";
+			
+			stmt = conn.prepareStatement(query);
+			
+			stmt.setString(1, user.getUserName());
+			stmt.setString(2, user.getfName());
+			stmt.setString(3, user.getlName());
+			stmt.setString(3, user.getEmail());
+			stmt.setString(4, user.getPassword());
+			stmt.setString(5, cal.getTime().toString());
+			stmt.setString(6, cal.getTime().toString());
+			
+			// returns total rows effected. 
+			if(stmt.executeUpdate() > 0)
+			{
+				result = true;
+			}
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			closeAll(stmt, conn);
+		}
+		return result;
+	}
+	
+	public static int deleteUser(String uName) 
+	{
+		int flag = 0;
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		if(uName == null || uName.length() <= 0)
+			return -1;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbUser, dbPass);
+			
+			stmt = conn.prepareStatement("delete from user where userName = ?");
+			stmt.setString(1, uName);
+			
+			flag = stmt.executeUpdate();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			closeAll(stmt, conn);
+		}
+		return flag;
+	}
+	
+	public static int modifyUser(User user)
+	{
+		int flag = 0;
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		if(user == null || !user.isValid())
+			return -1;
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbUser, dbPass);
+
+			String query = "update user set fName = ?, lName = ?, email = ?, pass = ?, isAdmin = ?, lastLogin = ? where userName = ?";
+			stmt = conn.prepareStatement(query);
+			
+			stmt.setString(1, user.getfName());
+			stmt.setString(2, user.getlName());
+			stmt.setString(3, user.getEmail());
+			stmt.setString(4, user.getPassword());
+			stmt.setString(5, user.getIsAdmin().toString());
+			
+			flag = stmt.executeUpdate();
+		}
+		catch (Exception e) {
+			e.printStackTrace();
+		}
+		finally {
+			closeAll(stmt, conn);
+		}
+		return flag;
+	}
+	
+	private static void closeAll(Statement stmt, Connection conn)
+	{
+		if(stmt != null) {
+			try {
+				stmt.close();
+			}
+			catch (SQLException sqle) {
+				sqle.printStackTrace();
+			}
+		}
+		
+		if(conn != null) {
+			try {
+				conn.close();
+			}
+			catch (SQLException sqle) {
+				sqle.printStackTrace();
+			}
+		}
+	}
+	
+	private static void closeAll(Statement stmt, Connection conn, ResultSet rs)
+	{
+		if(stmt != null) {
+			try {
+				stmt.close();
+			}
+			catch (SQLException sqle) {
+				sqle.printStackTrace();
+			}
+		}
+		
+		if(conn != null) {
+			try {
+				conn.close();
+			}
+			catch (SQLException sqle) {
+				sqle.printStackTrace();
+			}
+		}
+		
+		if(rs != null) {
+			try {
+				rs.close();
+			}
+			catch(SQLException sqle) {
+				sqle.printStackTrace();
+			}
+		}
 	}
 }
--- FinalProject/src/finalproject/models/User.java
+++ FinalProject/src/finalproject/models/User.java
@@ -1,21 +1,41 @@
 package finalproject.models;
 
+import java.sql.Date;
+
 public class User {
-	private String name = null;
-	private String password = null;
+	private String userName = null;
+	private String fName = null;
+	private String lName = null;
 	private String email = null;
+	private String password = null;
+	private Boolean isAdmin = false;
+	private Date lastLogin = null;
+	private Date accountCreated = null;
 	
 	public Boolean isValid()
 	{
-		return (name != null && email != null);
+		return (userName != null && fName != null 
+				&& lName != null && email != null);
 	}
 	
-	public String getName() {
-		return name;
+	public String getUserName() {
+		return userName;
 	}
 	
-	public void setName(String name) {
-		this.name = name;
+	public void setUserName(String uName) {
+		this.userName = uName;
+	}
+	
+	public String getfName() {
+		return fName;
+	}
+	
+	public void setfName(String name) {
+		this.fName = name;
+	}
+
+	public String getlName() {
+		return lName;
 	}
 	
 	public String getEmail() {
@@ -25,7 +45,7 @@ public class User {
 	public void setEmail(String mail) {
 		this.email = mail;
 	}
-
+	
 	public String getPassword() {
 		return password;
 	}
@@ -33,4 +53,29 @@ public class User {
 	public void setPassword(String password) {
 		this.password = password;
 	}
+	
+	public Date getLastLogin() {
+		return lastLogin;
+	}
+	
+	public void setLastLogin(Date date) {
+		this.lastLogin = date;
+	}
+	
+	public Date getAccountCreated() {
+		return accountCreated;
+	}
+	
+	public void setAccountCreated(Date date) {
+		this.accountCreated = date;
+	}
+	
+	public boolean getIsAdmin() {
+		return isAdmin;
+	}
+	
+	public void setIsAdmin(boolean admin) {
+		this.isAdmin = admin;
+	}
+	
 }
