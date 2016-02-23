f868cb8b4135fc0561c8535c1cbf02173d22c71d
Brett Holman <brettholman91@gmail.com>
2016-02-22 20:43:50 -0800

    Added best sellers query. Also fixed up other wonky query.

--- FinalProject/src/finalproject/data/inventoryDB.java
+++ FinalProject/src/finalproject/data/inventoryDB.java
@@ -6,6 +6,7 @@ import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
 import java.util.Calendar;
 
 import finalproject.models.Category;
@@ -17,6 +18,77 @@ public class inventoryDB {
 	private static String dbPass = "css490pass";
 	private static Calendar cal = Calendar.getInstance();
 	
+	// This will need some massive testing once the UI is using it. 
+	public static InventoryItem[] getBestSellers(int maxBooks, Category category)
+	{
+		// flag to tell if the request wants all categories or not. 
+		boolean allCategories = (category != null);
+		
+		ArrayList<InventoryItem> items = new ArrayList<InventoryItem>();
+		
+		Connection conn = null;
+		PreparedStatement stmt = null;
+		ResultSet rs = null;
+		
+		if(maxBooks <= 0)
+		{
+			return null;
+		}
+		
+		try {
+			Class.forName("com.mysql.jdbc.Driver");
+			conn = DriverManager.getConnection(dbURL, dbUser, dbPass);
+			
+			String query = "select pd.itemID, sum(pd.quantity) as total,"
+					+ "ii.id, ii.title, ii.quantity, ii.price, ii.description, c.categoryID, c.categoryName"
+					+ "from PurchaseDetails as pd"
+					+ "inner join InventoryItems as ii"
+					+ "on ii.id = pd.itoemID"
+					+ "inner join Category as c "
+					+ "on ii.categoryID = c.id"
+					+ (!allCategories ? "where ii.categoryID = ?" : "")
+					+ "group by pd.itemID" 
+					+ "order by total desc"
+					+ "limit ?;";
+
+			stmt = conn.prepareStatement(query);
+			
+			if(!allCategories)
+				stmt.setString(1, Integer.toString(category.getId()));
+			
+			stmt.setString(allCategories ? 1 : 2, Integer.toString(maxBooks));
+			
+			rs = stmt.executeQuery();
+			 
+			if(rs != null)
+			{
+				do 
+				{
+					InventoryItem item = new InventoryItem();
+					item.setId(rs.getInt("i.id"));
+					item.setTitle(rs.getNString("title"));
+					item.setQuantityInStock(rs.getInt("quantity"));
+					item.setDescription(rs.getNString("description"));
+					Category cat = new Category();
+					cat.setCategoryName(rs.getNString("categoryName"));
+					cat.setId(rs.getInt("c.id"));
+					item.setCategory(cat);
+					items.add(item);
+				}while(rs.next());
+			}
+			
+			
+		}
+		catch(Exception e) {
+			e.printStackTrace();
+			items = null;
+		}
+		finally {
+			closeAll(stmt, conn, rs);
+		}
+		return (InventoryItem[])items.toArray();
+	}
+
 	public static InventoryItem getInventoryItem(int itemID)
 	{
 		InventoryItem item = new InventoryItem();
@@ -30,10 +102,9 @@ public class inventoryDB {
 			Class.forName("com.mysql.jdbc.Driver");
 			conn = DriverManager.getConnection(dbURL, dbUser, dbPass);
 			
-			String query = "select i.id as 'i.id', i.title as 'i.title', "
-					+ "i.quantity as 'i.quantity', i.price as 'i.price', "
-					+ "i.categoryID as 'i.CategoryID', c.ID as 'c.ID', "
-					+ "c.categoryName as 'c.categoryName' "
+			String query = "select i.id, i.title,"
+					+ "i.quantity, i.price, i.categoryID, c.ID as 'c.ID', "
+					+ "c.categoryName"
 					+ "from InventoryItems as i "
 					+ "inner join Category as c "
 					+ "on c.id = i.categoryID "
@@ -50,11 +121,11 @@ public class inventoryDB {
 			
 			// Still need to check logic on this. 
 			item.setId(rs.getInt("i.id"));
-			item.setTitle(rs.getNString("i.title"));
-			item.setQuantityInStock(rs.getInt("i.quantity"));
-			item.setDescription(rs.getNString("i.description"));
+			item.setTitle(rs.getNString("title"));
+			item.setQuantityInStock(rs.getInt("quantity"));
+			item.setDescription(rs.getNString("description"));
 			Category cat = new Category();
-			cat.setCategoryName(rs.getNString("c.categoryName"));
+			cat.setCategoryName(rs.getNString("categoryName"));
 			cat.setId(rs.getInt("c.id"));
 			item.setCategory(cat);
 		}
